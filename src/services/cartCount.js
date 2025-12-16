22cce86e75c98e8028623fde8bb35cbae34f2270
Lalitpandey1 <154809463+Lalitpandey1@users.noreply.github.com>
2025-12-16 17:07:36 +0530

    revised

--- src/services/cartCount.js
+++ src/services/cartCount.js
@@ -1,10 +1,10 @@
 import { useSelector } from "react-redux";
 import { selectCartItems } from "../store/slices/cartSlice";
 
-const CartCount = () => {
+export default function CartCount() {
+  // Implementation here
+
   const Cart = useSelector(selectCartItems);
   const Count = Cart.reduce((acc, item) => (acc += item.Qty), 0);
   return Count;
-};
-
-export default CartCount;
+}
